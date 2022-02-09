f5375088dae8f9f95bdd80c24489751d2f785466
JosephChaaban <73365916+JosephChaaban@users.noreply.github.com>
2022-02-09 18:45:29 +0200

    Add files via upload

--- HW2/kernel.cu
+++ HW2/kernel.cu
@@ -60,7 +60,7 @@ void mm_gpu(float* A, float* B, float* C, unsigned int M, unsigned int N, unsign
 
     // TODO
 
-    dim3 numThreadsPerBlock(M,N);
+    dim3 numThreadsPerBlock(32,32);
     dim3 numBlocks((N+numThreadsPerBlock.x - 1)/numThreadsPerBlock.x, (M + numThreadsPerBlock.x - 1)/numThreadsPerBlock.x);
 
 
